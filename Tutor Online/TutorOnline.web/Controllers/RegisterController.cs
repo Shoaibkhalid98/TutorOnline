0127e9d2e75592ba87e30de8ab5b04ae169ecab2
Jawad Maqsood <37953839+jawad-maqsood@users.noreply.github.com>
2020-03-13 21:57:36 +0500

    working on validations

--- Tutor Online/TutorOnline.web/Controllers/RegisterController.cs
+++ Tutor Online/TutorOnline.web/Controllers/RegisterController.cs
@@ -16,27 +16,31 @@ namespace TutorOnline.web.Controllers
         [HttpPost]
         public ActionResult RegisterStudent(RegisterStudentViewModel model)
         {
-            //extract data from model to login table
-            var login = new Login();
-            login.Adress = model.Adress;
-            login.Description = model.Description;
-            login.Email = model.Email;
-            login.imageUrl = model.imageUrl;
-            login.Name = model.Name;
-            login.Password = model.Password;
-            login.phone = model.phone;
-            login.userType = model.userType;
-            //extract data from model to Student table
-            var student = new Student();
-            student.Adress = login.Adress;
-            student.Description = login.Description;
-            student.Email = login.Email;
-            student.imageUrl = login.imageUrl;
-            student.Name = login.Name;
-            student.Password = login.Password;
-            student.phone = login.phone;
-            LoginServices.Instance.SaveLogin(login);
-            StudentServices.Instance.SaveStudent(student);
+            if (ModelState.IsValid)
+            {
+                //extract data from model to login table
+                var login = new Login();
+                login.Adress = model.Adress;
+                login.Description = model.Description;
+                login.Email = model.Email;
+                login.imageUrl = model.imageUrl;
+                login.Name = model.Name;
+                login.Password = model.Password;
+                login.phone = model.phone;
+                login.userType = model.userType;
+                //extract data from model to Student table
+                var student = new Student();
+                student.Adress = login.Adress;
+                student.Description = login.Description;
+                student.Email = login.Email;
+                student.imageUrl = login.imageUrl;
+                student.Name = login.Name;
+                student.Password = login.Password;
+                student.phone = login.phone;
+                LoginServices.Instance.SaveLogin(login);
+                StudentServices.Instance.SaveStudent(student);
+                
+            }
             return View();
         }
 
@@ -48,32 +52,34 @@ namespace TutorOnline.web.Controllers
         [HttpPost]
         public ActionResult RegisterTeacher(RegisterTeacherViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                //extract data from model to login table
+                var login = new Login();
+                login.Adress = model.Adress;
+                login.Description = model.Description;
+                login.Email = model.Email;
+                login.imageUrl = model.imageUrl;
+                login.Name = model.Name;
+                login.Password = model.Password;
+                login.phone = model.phone;
+                login.userType = model.userType;
 
-            //extract data from model to login table
-            var login = new Login();
-            login.Adress = model.Adress;
-            login.Description = model.Description;
-            login.Email = model.Email;
-            login.imageUrl = model.imageUrl;
-            login.Name = model.Name;
-            login.Password = model.Password;
-            login.phone = model.phone;
-            login.userType = model.userType;
+                //extract data from model to login table
+                var teacher = new Teacher();
+                teacher.Adress = model.Adress;
+                teacher.Description = model.Description;
+                teacher.Email = model.Email;
+                teacher.imageUrl = model.imageUrl;
+                teacher.Name = model.Name;
+                teacher.Password = model.Password;
+                teacher.phone = model.phone;
+                teacher.DegreeImageUrl = model.DegreeImageUrl;
+                teacher.currentAccountStatus = "UnVerified";
 
-            //extract data from model to login table
-            var teacher = new Teacher();
-            teacher.Adress = model.Adress;
-            teacher.Description = model.Description;
-            teacher.Email = model.Email;
-            teacher.imageUrl = model.imageUrl;
-            teacher.Name = model.Name;
-            teacher.Password = model.Password;
-            teacher.phone = model.phone;
-            teacher.DegreeImageUrl = model.DegreeImageUrl;
-            teacher.currentAccountStatus = "UnVerified";
-
-            LoginServices.Instance.SaveLogin(login);
-            TeacherServices.Instance.SaveTeacher(teacher);
+                LoginServices.Instance.SaveLogin(login);
+                TeacherServices.Instance.SaveTeacher(teacher);
+            }
             return View();
         }
         [HttpGet]
